7bb4d325ee80b27046e917eabf73af8327bf7920
Garvit1000 <garvitjoshi543@gmail.com>
2026-02-15 17:45:36 +0530

    resolve errors 1

--- dist/client/assets/collections._handle-EALsBTsQ.js
+++
@@ -1,1 +0,0 @@
-import{j as o,N as n}from"./main-DBXAVEaL.js";const s=()=>o.jsx(n,{children:"Collection not found"});export{s as notFoundComponent};
--- dist/client/assets/shopify-BmbBy14K.js
+++
@@ -1,1 +0,0 @@
-function t(r,e){return new Intl.NumberFormat("en-US",{style:"currency",currency:e}).format(parseFloat(r))}export{t as f};
